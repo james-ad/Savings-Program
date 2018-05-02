1e7b8601503afaadf492922549b06dc30fd5b3f7
james-ad <muzikjay@gmail.com>
2018-05-02 11:38:12 -0700

    multiple functional changes/fixes and styling changes

--- script.js
+++ script.js
@@ -24,30 +24,37 @@ window.addEventListener('load', function() {
     });
 
 // Show savings amount when user either enters in a check amount or adjusts the savings percentage using the slider
-    checkAmount.addEventListener('input', function() {
-        moneySaved.innerHTML = convertPercentage(checkAmount.value).toFixed(2);
-    });
-
     percentage.addEventListener('input', function() {
         sliderReadout.innerHTML = percentage.value + ' %';
-        moneySaved.innerHTML = convertPercentage(checkAmount.value).toFixed(2);
+        moneySaved.innerHTML = '$' + convertPercentage(checkAmount.value).toFixed(2);
     });
+
+    checkAmount.addEventListener('input', function() {
+        moneySaved.innerHTML = '$' + convertPercentage(checkAmount.value).toFixed(2);
+    });
+
 
 // Remove selected entry and clear check history upon clicking appropriate buttons
     document.addEventListener('click', function(e) {
         let target = e.target;
-        if (e.target.className == 'remove-check') {
-                target.parentNode.parentNode.remove();
-                // find value in Savings category of table, remove it from the totalSavings array and update the Savings Total section of the page.
-                for (i=0; i < totalSavings.length; i++) {
-                    let rmValue = target.parentNode.previousSibling.value;
-                    if (rmValue == totalSavings[i]) {
-                        let index = totalSavings.indexOf(rmValue);
-                        totalSavings.splice(index, 1);
-                    }
+        if (target.className === 'remove-check') {
+            target.parentNode.parentNode.remove();
+            // find value in Savings category of table, remove it from the totalSavings array and update the Savings Total section of the page.
+            for (i=0; i < totalSavings.length; i++) {
+                let rmValue = target.parentNode.previousSibling.previousSibling.innerText;
+                if (rmValue == totalSavings[i]) {
+                    let index = totalSavings.indexOf(rmValue);
+                    totalSavings.splice(index, 1);
+                    break;
                 }
-                console.log(totalSavings);
             }
+            console.log(totalSavings);
+            if (totalSavings.length > 0) {
+            savings.innerHTML = '$' + totalSavings.reduce(function(a, b) {
+                return a + b;
+            }).toFixed(2)
+            }
+        }
     });
 
     clearCheckHistory.addEventListener('click', function() {
@@ -55,8 +62,9 @@ window.addEventListener('load', function() {
         checks.deleteRow(i);
         }
         checkHistory = [];
-        savings.innerHTML = '';
+        savings.innerHTML = '$0.00';
         console.log(checkHistory);
+        console.log(checks.rows.length);
     });
 
 // Add each new check deposited onto the beginning of an array  
@@ -65,15 +73,16 @@ window.addEventListener('load', function() {
 //  Create funcitons that update HTML with new check history
     function depositCheck() {
         checkHistory.unshift(checkAmount.value);
-        checks.innerHTML += '<tr class="entry"> <td>' + checkAmount.value + '</td>' + '<td>' + percentage.value + ' %' + '</td>' + '<td class="savings-class">' + convertPercentage(checkAmount.value).toFixed(2) + '</td> <td><button class="remove-check">X</button> </td></tr>';
+        checks.innerHTML += '<tr class="entry"> <td>' + '$' + numberWithCommas(checkAmount.value) + '</td>' + '<td>' + percentage.value + ' %' + '</td>' + '<td class="savings-class">' + '$' + numberWithCommas(convertPercentage(checkAmount.value).toFixed(2)) + '</td> <td><button class="remove-check">X</button> </td></tr>';
         
         totalSavings.push(Number(convertPercentage(checkAmount.value).toFixed(2)));
         console.log(totalSavings);
-        savings.innerHTML = totalSavings.reduce(function(a, b) {
+        savings.innerHTML = '$' + numberWithCommas(totalSavings.reduce(function(a, b) {
             return a + b;
-        });
+        }).toFixed(2));
         console.log('check collection: ' + checkHistory);
         console.log('savings collection: ' + totalSavings);
+        console.log(checks.rows.length);
     }
 
     function convertPercentage(n) {
@@ -81,5 +90,8 @@ window.addEventListener('load', function() {
         return sliderValue * .01 * n;
     }
     
+    const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 });
